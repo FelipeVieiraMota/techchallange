2b8342c3e577183bbf32421d15a55c31f6d5b5cb
João Paulo de Lima <joao.lima_dev@outlook.com>
2024-08-12 19:00:57 -0300

    Ajustes faltantes

--- findmyjob.v1/src/main/java/api/dto/CandidatoDTO.java
+++ findmyjob.v1/src/main/java/api/dto/CandidatoDTO.java
@@ -4,7 +4,6 @@ import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Size;
 import org.hibernate.validator.constraints.NotBlank;
-
 import java.util.Date;
 
 public record CandidatoDTO (
